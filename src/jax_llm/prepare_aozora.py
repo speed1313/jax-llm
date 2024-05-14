e2c0415a6dc1d819a94b47c7bf1a64d4399315e2
speed1313 <speedtry13@icloud.com>
2024-05-14 14:13:50 +0900

    refactoring

--- src/jax_llm/prepare_aozora.py
+++ src/jax_llm/prepare_aozora.py
@@ -5,11 +5,17 @@ import click
 @click.command()
 @click.option("--book_num", type=int, default=10)
 def main(book_num: int):
-    ds = load_dataset("globis-university/aozorabunko-clean", cache_dir="data")
+    import os
+
+    os.makedirs("data/aozora", exist_ok=True)
+    save_path = "data/aozora/input.txt"
+    ds = load_dataset(
+        "globis-university/aozorabunko-clean", cache_dir="data/aozora/cache"
+    )
     ds = ds.filter(lambda row: row["meta"]["文字遣い種別"] == "新字新仮名")
     # concat each bokk with <|endoftext|> token
     print(len(ds["train"]))
-    with open("input.txt", "w") as f:
+    with open(save_path, "w") as f:
         for i, book in enumerate(ds["train"]):
             if i > book_num:
                 break
--- src/jax_llm/train.py
+++ src/jax_llm/train.py
@@ -7,193 +7,232 @@ from tokenizers import Tokenizer
 from model import NanoLM
 import pickle
 import json
-
-# platform check
-print("JAX running on", jax.devices()[0].platform.upper())
-
-# @markdown Random seed:
-SEED = 42  # @param{type:"integer"}
-# @markdown Learning rate passed to the optimizer:
-LEARNING_RATE = 1e-4  # @param{type:"number"}
-# @markdown Batch size:
-BATCH_SIZE = 256  # @param{type:"integer"}
-# @markdown Numer of training iterations:
-N_ITERATIONS = 50000  # @param{type:"integer"}
-# @markdown Number of training iterations between two consecutive evaluations:
-N_FREQ_EVAL = 1000  # @param{type:"integer"}
-# @markdown Rate for dropout in the transformer model
-DROPOUT_RATE = 0.2  # @param{type:"number"}
-# @markdown Context window for the transformer model
-BLOCK_SIZE = 64  # @param{type:"integer"}
-# @markdown Number of layer for the transformer model
-NUM_LAYERS = 6  # @param{type:"integer"}
-# @markdown Size of the embedding for the transformer model
-EMBED_SIZE = 256  # @param{type:"integer"}
-# @markdown Number of heads for the transformer model
-NUM_HEADS = 8  # @param{type:"integer"}
-# @markdown Size of the heads for the transformer model
-HEAD_SIZE = 32  # @param{type:"integer"}
+import click
 
 
-key = jax.random.PRNGKey(0)
-
-tokenizer = AbstractTokenizer(
-    Tokenizer.from_file("data/tokenizer.json"), "data/tokenizer.json"
+@click.command()
+@click.option("--data_name", type=str, default="aozora")
+@click.option("--seed", type=int, default=42)
+@click.option("--learning_rate", type=float, default=1e-4)
+@click.option("--batch_size", type=int, default=256)
+@click.option(
+    "--n_iterations",
+    type=int,
+    default=50000,
+    help="Number of training iterations (batch_size * block_size * n_iterations = total tokens seen)",
 )
-print("Tokenizer loaded")
-with open("input.txt", "r", encoding="utf-8") as f:
-    text_data = f.read()
-train_ratio = 0.90
-total_tokens = len(tokenizer.encode(text_data))
-split_idx = int(train_ratio * len(text_data))
-train_data = text_data[:split_idx]
-train_data = tokenizer.encode(train_data)
-train_data = jnp.array(train_data)
-eval_data = text_data[split_idx:]
-eval_data = tokenizer.encode(eval_data)
-eval_data = jnp.array(eval_data)
+@click.option(
+    "--n_freq_eval",
+    type=int,
+    default=1000,
+    help="Number of training iterations between two consecutive evaluations",
+)
+@click.option("--dropout_rate", type=float, default=0.2)
+@click.option(
+    "--block_size",
+    type=int,
+    default=64,
+    help="Context window for the transformer model",
+)
+@click.option(
+    "--num_layers",
+    type=int,
+    default=6,
+    help="Number of layer for the transformer model",
+)
+@click.option(
+    "--embed_size",
+    type=int,
+    default=256,
+    help="Size of the embedding for the transformer model",
+)
+@click.option(
+    "--num_heads", type=int, default=8, help="Number of heads for the transformer model"
+)
+@click.option(
+    "--head_size",
+    type=int,
+    default=32,
+    help="Size of the heads for the transformer model",
+)
+def main(
+    data_name: str,
+    seed: int,
+    learning_rate: float,
+    batch_size: int,
+    n_iterations: int,
+    n_freq_eval: int,
+    dropout_rate: float,
+    block_size: int,
+    num_layers: int,
+    embed_size: int,
+    num_heads: int,
+    head_size: int,
+):
+    data_filename = f"data/{data_name}/input.txt"
+    tokenizer_path = f"data/{data_name}/tokenizer.json"
+    model_path = f"model/{data_name}"
+    import os
 
+    os.makedirs(model_path, exist_ok=True)
 
-dynamic_slice_vmap = jax.vmap(jax.lax.dynamic_slice, in_axes=(None, 0, None))
+    # platform check
+    print("JAX running on", jax.devices()[0].platform.upper())
 
-
-@jax.jit
-def get_batch(random_key, data):
-    """Prepares a random batch of training data.
-
-    Args:
-        random_key: A random seed for sampling a batch.
-        data: The complete training dataset.
-
-    Returns:
-        x: Input sequences.
-        y: Target sequences (shifted inputs).
-    """
-    ix = jax.random.randint(
-        random_key, shape=(BATCH_SIZE, 1), minval=0, maxval=len(data) - BLOCK_SIZE
+    tokenizer = AbstractTokenizer(
+        Tokenizer.from_file(tokenizer_path), tokenizer_path.split("/")[-2]
     )
-    x = dynamic_slice_vmap(data, ix, (BLOCK_SIZE,))
-    y = dynamic_slice_vmap(data, ix + 1, (BLOCK_SIZE,))
-    return x, y
+    print("Tokenizer loaded")
+    # load tokenizer json
 
+    tokenizer_json = json.load(open(tokenizer_path, "r"))
+    vocab_size = len(tokenizer_json["model"]["vocab"])
 
-model = NanoLM(
-    vocab_size=30000,
-    num_layers=NUM_LAYERS,
-    num_heads=NUM_HEADS,
-    head_size=HEAD_SIZE,
-    dropout_rate=DROPOUT_RATE,
-    embed_size=EMBED_SIZE,
-    block_size=BLOCK_SIZE,
-)
+    with open(data_filename, "r", encoding="utf-8") as f:
+        text_data = f.read()
+    train_ratio = 0.90
+    total_tokens = len(tokenizer.encode(text_data))
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    train_data = tokenizer.encode(train_data)
+    train_data = jnp.array(train_data)
+    eval_data = text_data[split_idx:]
+    eval_data = tokenizer.encode(eval_data)
+    eval_data = jnp.array(eval_data)
 
+    dynamic_slice_vmap = jax.vmap(jax.lax.dynamic_slice, in_axes=(None, 0, None))
 
-def loss_fun(params, x, y, dropout_key):
-    logits = model.apply(params, x, training=True, rngs={"dropout": dropout_key})
-    return optax.softmax_cross_entropy_with_integer_labels(
-        logits=logits, labels=y
-    ).mean()
+    @jax.jit
+    def get_batch(random_key, data):
+        """Prepares a random batch of training data.
 
+        Args:
+            random_key: A random seed for sampling a batch.
+            data: The complete training dataset.
 
-@jax.jit
-def eval_step(params, x, y):
-    logits = model.apply(params, x, training=False)
-    return optax.softmax_cross_entropy_with_integer_labels(
-        logits=logits, labels=y
-    ).mean()
+        Returns:
+            x: Input sequences.
+            y: Target sequences (shifted inputs).
+        """
+        ix = jax.random.randint(
+            random_key, shape=(batch_size, 1), minval=0, maxval=len(data) - block_size
+        )
+        x = dynamic_slice_vmap(data, ix, (block_size,))
+        y = dynamic_slice_vmap(data, ix + 1, (block_size,))
+        return x, y
 
+    model = NanoLM(
+        vocab_size=vocab_size,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        head_size=head_size,
+        dropout_rate=dropout_rate,
+        embed_size=embed_size,
+        block_size=block_size,
+    )
 
-key = jax.random.PRNGKey(SEED)
-key, subkey = jax.random.split(key)
+    def loss_fun(params, x, y, dropout_key):
+        logits = model.apply(params, x, training=True, rngs={"dropout": dropout_key})
+        return optax.softmax_cross_entropy_with_integer_labels(
+            logits=logits, labels=y
+        ).mean()
 
-var_params = model.init(
-    key,
-    jnp.ones((BATCH_SIZE, BLOCK_SIZE), dtype=jnp.int32),
-    training=False,
-)
+    @jax.jit
+    def eval_step(params, x, y):
+        logits = model.apply(params, x, training=False)
+        return optax.softmax_cross_entropy_with_integer_labels(
+            logits=logits, labels=y
+        ).mean()
 
-
-n_params = sum(p.size for p in jax.tree_util.tree_leaves(var_params))
-
-print(f"Total number of parameters: {n_params:_}")
-
-
-# To run with SGD instead of adam, replace `adam` with `sgd`
-opt = optax.adamw(learning_rate=LEARNING_RATE)
-
-opt_state = opt.init(var_params)
-
-
-all_train_losses = []
-all_eval_losses = []
-
-
-# we define one iteration of the optimizer and JIT this function
-@jax.jit
-def step(key, params, opt_state):
+    key = jax.random.PRNGKey(SEED)
     key, subkey = jax.random.split(key)
-    batch = get_batch(key, train_data)
-    loss, grad = jax.value_and_grad(loss_fun)(params, *batch, subkey)
-    updates, opt_state = opt.update(grad, opt_state, params)
-    params = optax.apply_updates(params, updates)
-    return params, key, opt_state, loss
 
+    var_params = model.init(
+        key,
+        jnp.ones((batch_size, block_size), dtype=jnp.int32),
+        training=False,
+    )
 
-for i in range(N_ITERATIONS):
-    var_params, key, opt_state, loss = step(key, var_params, opt_state)
-    all_train_losses.append(loss)
+    n_params = sum(p.size for p in jax.tree_util.tree_leaves(var_params))
 
-    # once every N_FREQ_EVAL we compute loss on the validation set
-    if i % N_FREQ_EVAL == 0:
+    print(f"Total number of parameters: {n_params:_}")
+
+    # To run with SGD instead of adam, replace `adam` with `sgd`
+    opt = optax.adamw(learning_rate=learning_rate)
+
+    opt_state = opt.init(var_params)
+
+    all_train_losses = []
+    all_eval_losses = []
+
+    # we define one iteration of the optimizer and JIT this function
+    @jax.jit
+    def step(key, params, opt_state):
         key, subkey = jax.random.split(key)
-        eval_loss = eval_step(var_params, *get_batch(subkey, eval_data))
-        all_eval_losses.append(eval_loss)
-        print(f"Step: {i}\t train loss: {loss}\t eval loss: {eval_loss}")
+        batch = get_batch(key, train_data)
+        loss, grad = jax.value_and_grad(loss_fun)(params, *batch, subkey)
+        updates, opt_state = opt.update(grad, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return params, key, opt_state, loss
+
+    for i in range(n_iterations):
+        var_params, key, opt_state, loss = step(key, var_params, opt_state)
+        all_train_losses.append(loss)
+
+        # once every n_freq_eval we compute loss on the validation set
+        if i % n_freq_eval == 0:
+            key, subkey = jax.random.split(key)
+            eval_loss = eval_step(var_params, *get_batch(subkey, eval_data))
+            all_eval_losses.append(eval_loss)
+            print(f"Step: {i}\t train loss: {loss}\t eval loss: {eval_loss}")
+
+    plt.title("Convergence of adamw (train loss)")
+    plt.plot(all_train_losses, label="train", lw=3)
+    plt.plot(
+        jnp.arange(0, len(all_eval_losses) * n_freq_eval, n_freq_eval),
+        all_eval_losses,
+        label="test",
+        lw=3,
+    )
+    # display token seen num on the upper x-axis
+    ax2 = plt.gca().twiny()
+    tokens_seen = jnp.arange(0, len(all_train_losses)) * batch_size * block_size
+    ax2.plot(tokens_seen, all_train_losses, alpha=0)
+    ax2.set_xlabel("tokens seen")
+    plt.xlabel("steps")
+    plt.ylabel("loss")
+    plt.grid()
+    plt.legend(frameon=False)
+    plt.savefig("train_loss.png")
+    plt.show()
+
+    # Let's now generate some text
+    key, subkey = jax.random.split(key)
+    text = model.generate(key, var_params, 1000)[:, 0, 0].tolist()
+    print(tokenizer.decode(text))
+
+    # Store the model
+    with open(f"{model_path}/params.pkl", "wb") as f:
+        pickle.dump(var_params, f)
+    print("Params stored")
+
+    # Store optimizer state
+    with open(f"{model_path}/opt_state.pkl", "wb") as f:
+        pickle.dump(opt_state, f)
+
+    # store the moel config
+    config = {
+        "vocab_size": vocab_size,
+        "num_layers": num_layers,
+        "num_heads": num_heads,
+        "head_size": head_size,
+        "dropout_rate": dropout_rate,
+        "embed_size": embed_size,
+        "block_size": block_size,
+    }
+    with open(f"{model_path}/config.json", "w") as f:
+        json.dump(config, f)
 
 
-plt.title("Convergence of adamw (train loss)")
-plt.plot(all_train_losses, label="train", lw=3)
-plt.plot(
-    jnp.arange(0, len(all_eval_losses) * N_FREQ_EVAL, N_FREQ_EVAL),
-    all_eval_losses,
-    label="test",
-    lw=3,
-)
-# display token seen num on the upper x-axis
-ax2 = plt.gca().twiny()
-tokens_seen = jnp.arange(0, len(all_train_losses)) * BATCH_SIZE * BLOCK_SIZE
-ax2.plot(tokens_seen, all_train_losses, alpha=0)
-ax2.set_xlabel("tokens seen")
-plt.xlabel("steps")
-plt.ylabel("loss")
-plt.grid()
-plt.legend(frameon=False)
-plt.savefig("train_loss.png")
-plt.show()
-
-
-# Let's now generate some text
-key, subkey = jax.random.split(key)
-text = model.generate(key, var_params, 1000)[:, 0, 0].tolist()
-print(tokenizer.decode(text))
-
-# Store the model
-import os
-
-os.makedirs("model", exist_ok=True)
-with open("model/params.pkl", "wb") as f:
-    pickle.dump(var_params, f)
-print("Params stored")
-
-# store the moel config
-config = {
-    "vocab_size": 30000,
-    "num_layers": NUM_LAYERS,
-    "num_heads": NUM_HEADS,
-    "head_size": HEAD_SIZE,
-    "dropout_rate": DROPOUT_RATE,
-    "embed_size": EMBED_SIZE,
-    "block_size": BLOCK_SIZE,
-}
-with open("model/config.json", "w") as f:
-    json.dump(config, f)
+if __name__ == "__main__":
+    main()
